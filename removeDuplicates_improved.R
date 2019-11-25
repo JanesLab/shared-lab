c2a124841641455771c5f5423a9088e58fd53cb9
mdsutcliffe <mdsutcliffe5@gmail.com>
2019-11-25 11:14:40 -0500

    Improve speed

--- removeDuplicates_improved.R
+++ removeDuplicates_improved.R
@@ -4,16 +4,20 @@ removeDuplicates_improved <- function(rsem, index_counts) {
 
   duplicatedSymbols <- unique(rsem$symbol[duplicated(rsem$symbol)])
   indsToDelete <- numeric(0)
-  for (iSymbol in duplicatedSymbols) {
-    duplicatedSymbolRows <- which(rsem$symbol == iSymbol)
+  indsToKeep <- numeric(0)
+  # Pre-allocate matrix for colSum-ed rows
+  editVals <- matrix(data = rep(0,length(index_counts)*length(duplicatedSymbols)),nrow = length(duplicatedSymbols),ncol = length(index_counts))
+  for (iSymbol in 1:length(duplicatedSymbols)) {
+    duplicatedSymbolRows <- which(rsem$symbol == duplicatedSymbols[iSymbol])
     
     # Save row that has the shortest chromosome name / lowest Ensembl ID
     indPriority <- order(nchar(rsem$chr[duplicatedSymbolRows]),rsem$ensgene[duplicatedSymbolRows])
     
-    indToKeep <- duplicatedSymbolRows[indPriority[1]]
+    indsToKeep <- c(indsToKeep,duplicatedSymbolRows[indPriority[1]])
     indsToDelete <- c(indsToDelete,duplicatedSymbolRows[indPriority[2:length(indPriority)]])
-    rsem[indToKeep,index_counts] <- colSums(rsem[duplicatedSymbolRows,index_counts])
+    editVals[iSymbol,] <- colSums(rsem[duplicatedSymbolRows,index_counts])
   }
+  rsem[indsToKeep,index_counts] <- editVals
   rsem <- rsem[-indsToDelete,]
   
   return(rsem)
